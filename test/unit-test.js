ff481c939972e224ea64efdb2caa53e46424781b
Andy Beeching <andybeeching@gmail.com>
2012-11-08 21:13:52 +0100

    Added example BDD spec tests

--- test/unit-test.js
+++ test/unit-test.js
@@ -1,4 +1,4 @@
-/*global buster, assert*/
+/*global buster, assert, describe, it*/
 
 (function (global) {
 
@@ -7,10 +7,34 @@
   var buster = global.buster || require( "buster" );
   var SUT = global.SUT || require( "../lib/sut" );
 
-  buster.testCase( "SUT Basic", {
-    "SUT.sum() is testable": function () {
+  buster.testCase( "SUT", {
+    // Example Test
+    "foo": function () {
       assert( SUT.sum(1, 2), 3 );
+    },
+
+    // Second Example Test
+    "nest": {
+      "bar": function () {
+        assert( SUT.sum(3,4), 7);
+      }
     }
+  });
+
+  buster.spec.expose();
+
+  var spec = describe( "SUT", function () {
+    // Example Feature
+    it("should foo", function () {
+      assert( SUT.sum(1, 2), 3 );
+    });
+
+    // Example Nested Feature
+    describe("nest", function () {
+      it("should bar", function () {
+        assert( SUT.sum(3,4), 7);
+      });
+    });
   });
 
 }(this));
